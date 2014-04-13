38ff937d77147b300c147d92ef352c3b86c5d78a
Christian Forster <forster@ifi.uzh.ch>
2014-04-13 19:20:32 +0200

    handle parameter files with nested dictionaries

--- vikit_py/src/vikit_py/ros_node.py
+++ vikit_py/src/vikit_py/ros_node.py
@@ -3,14 +3,21 @@
 import os
 
 class RosNode:
-  def __init__(self, package, executable):
-    self._package = package
-    self._executable = executable
+    def __init__(self, package, executable):
+        self._package = package
+        self._executable = executable
+        self._param_string = ''
     
-  def run(self, parameter_dictionary):
-    parameter_string = ''
-    for key in parameter_dictionary.keys():
-      parameter_string = parameter_string + ' _' + key + ':=' + str(parameter_dictionary[key])
-    print('Starting ROS node.')
-    os.system('rosrun ' + self._package + ' ' + self._executable + ' ' + parameter_string)
-    print('ROS node finished processing.')
+    def add_parameters(self, namespace, parameter_dictionary):
+        for key in parameter_dictionary.keys():
+            if type(parameter_dictionary[key]) is dict:
+                self.add_parameters(namespace+key+'/', parameter_dictionary[key])
+            else:
+                self._param_string += ' _'+namespace+key+':='+str(parameter_dictionary[key])
+        
+    def run(self, parameter_dictionary, namespace=''):
+        self.add_parameters(namespace, parameter_dictionary)
+        print('Starting ROS node with parameters: '+self._param_string)
+        
+        os.system('rosrun ' + self._package + ' ' + self._executable + ' ' + self._param_string)
+        print('ROS node finished processing.')
