4271a28265f02c507472a1f5667922c25bb32edd
Christian Forster <forster@ifi.uzh.ch>
2014-05-13 12:47:06 +0200

    added blender_utils function to estimate depthmap normal

--- vikit_common/include/vikit/blender_utils.h
+++ vikit_common/include/vikit/blender_utils.h
@@ -13,13 +13,15 @@
 #include <vikit/math_utils.h>
 #include <opencv2/opencv.hpp>
 #include <fstream>
+#include <Eigen/Core>
 
 namespace vk {
 namespace blender_utils {
 
-void loadBlenderDepthmap(const std::string file_name,
-                         const vk::AbstractCamera& cam,
-                         cv::Mat& img)
+void loadBlenderDepthmap(
+    const std::string file_name,
+    const vk::AbstractCamera& cam,
+    cv::Mat& img)
 {
   std::ifstream file_stream(file_name.c_str());
   assert(file_stream.is_open());
@@ -42,6 +44,42 @@ void loadBlenderDepthmap(const std::string file_name,
         printf("WARNING: did not read the full depthmap!\n");
     }
   }
+}
+
+bool getDepthmapNormalAtPoint(
+    const Vector2i& px,
+    const cv::Mat& depth,
+    const int halfpatch_size,
+    const vk::AbstractCamera& cam,
+    Vector3d& normal)
+{
+  assert(cam.width() == depth.cols && cam.height() == depth.rows);
+  if(!cam.isInFrame(px, halfpatch_size+1))
+    return false;
+
+  const size_t n_meas = (halfpatch_size*2+1)*(halfpatch_size*2+1);
+  list<Vector3d> pts;
+  for(int y = px[1]-halfpatch_size; y<=px[1]+halfpatch_size; ++y)
+    for(int x = px[0]-halfpatch_size; x<=px[0]+halfpatch_size; ++x)
+      pts.push_back(cam.cam2world(x,y)*depth.at<float>(y,x));
+
+  assert(n_meas == pts.size());
+  Matrix<double, Dynamic, 4> A; A.resize(n_meas, Eigen::NoChange);
+  Matrix<double, Dynamic, 1> b; b.resize(n_meas, Eigen::NoChange);
+
+  size_t i = 0;
+  std::for_each(pts.begin(), pts.end(), [&](Vector3d& pt){
+    A.row(i) << pt.x(), pt.y(), pt.z(), 1.0;
+    b[i] = 0;
+    ++i;
+  });
+
+  JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
+
+  Matrix<double, 4, 4> V = svd.matrixV();
+  normal = V.block<3,1>(0,3);
+  normal.normalize();
+  return true;
 }
 
 namespace file_format
