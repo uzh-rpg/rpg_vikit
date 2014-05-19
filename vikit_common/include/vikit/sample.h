073db77b1786e884368d3398a962cf591709b4d9
Christian Forster <ch.forster@gmail.com>
2014-05-19 12:03:25 +0200

    removed tr1. new function for initializing the generators with a seed.

--- vikit_common/include/vikit/sample.h
+++ vikit_common/include/vikit/sample.h
@@ -1,73 +1,50 @@
-// This file is part of VisionTools.
-//
-// Copyright 2011 Hauke Strasdat (Imperial College London)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
+#ifndef VIKIT_SAMPLE_H_
+#define VIKIT_SAMPLE_H_
 
-#ifndef SAMPLE_H_
-#define SAMPLE_H_
-
-#include <tr1/random>
+#include <random>
+#include <chrono>
 
 namespace vk {
 
 class Sample
 {
 public:
-  static int
-  uniform                    (int from, int to);
-  static double
-  uniform                    ();
-  static double
-  gaussian                   (double sigma);
-
-  static tr1::ranlux_base_01 gen_real;
-  static tr1::mt19937 gen_int;
+  static void setTimeBasedSeed();
+  static int uniform(int from, int to);
+  static double uniform();
+  static double gaussian(double sigma);
+  static std::ranlux24 gen_real;
+  static std::mt19937 gen_int;
 };
 
-tr1::ranlux_base_01 Sample::gen_real;
-tr1::mt19937 Sample::gen_int;
+std::ranlux24 Sample::gen_real;
+std::mt19937 Sample::gen_int;
 
-int Sample
-::uniform(int from, int to)
+void Sample::setTimeBasedSeed()
 {
-  tr1::uniform_int<int> unif(from, to);
-  int sample = unif(gen_int);
-  return sample;
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  gen_real = std::ranlux24(seed);
+  gen_int = std::mt19937(seed);
 }
 
-double Sample
-::uniform()
+int Sample::uniform(int from, int to)
 {
-  tr1::uniform_real<double> unif(0.0, 1.0);
-  double sample = unif(gen_real);
-  return sample;
+  std::uniform_int_distribution<int> distribution(from, to);
+  return distribution(gen_int);
 }
 
-double Sample
-::gaussian(double sigma)
+double Sample::uniform()
 {
-  tr1::normal_distribution<double> gauss(0.0, sigma);
-  double sample = gauss(gen_real);
-  return sample;
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  return distribution(gen_real);
 }
 
+double Sample::gaussian(double stddev)
+{
+  std::normal_distribution<double> distribution(0.0, stddev);
+  return distribution(gen_real);
 }
 
-#endif /* SAMPLE_H_ */
+} // namespace vk
+
+#endif // VIKIT_SAMPLE_H_
